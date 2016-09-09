f69767c91a869e6db388e23d4311266a9e6f8bca
prawan <kantima.praw57@sskru.ac.th>
2016-09-09 14:30:40 +0700

    new project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello kantima");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any Key to eixt.");
+            Console.ReadKey();
+        }
+    }
+}
